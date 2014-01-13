c12730e4793bbdf98370390ec1af59db4df37546
Michael Mandato <michaelmandato@dreamfactory.com>
2014-01-13 09:30:55 -0500

    update

--- app/scripts/animations/animations.js
+++ app/scripts/animations/animations.js
@@ -104,23 +104,23 @@ lpApp.animation('.panel-swipe', ['$rootScope', function($rootScope) {
             if ($rootScope.panelSwipe === 'left') {
                 jQuery(element).css({
                     opacity: 1,
-                    transform: 'scale3d(1, 1, 1)'
+                    transform: 'scale(1, 1)'
                 });
 
                 jQuery(element).animate({
                     opacity: 0,
-                    transform: 'scale3d(2, 2, 1)'
-                }, 750, done);
+                    transform: 'scale(2, 2)'
+                }, 600, done);
             }else {
                 jQuery(element).css({
                     opacity: 1,
-                    transform: 'scale3d(1, 1, 1)'
+                    transform: 'scale(1, 1)'
                 });
 
                 jQuery(element).animate({
                     opacity: 0,
-                    transform: 'scale3d(0, 0, 1)'
-                }, 750, done);
+                    transform: 'scale(0, 0)'
+                }, 600, done);
             }
 
         },
@@ -129,23 +129,23 @@ lpApp.animation('.panel-swipe', ['$rootScope', function($rootScope) {
             if ($rootScope.panelSwipe === 'left') {
                 jQuery(element).css({
                     opacity: 0,
-                    transform: 'scale3d(0, 0, 1)'
+                    transform: 'scale(0, 0)'
                 });
 
                 jQuery(element).animate({
                     opacity: 1,
-                    transform: 'scale3d(1, 1, 1)'
-                }, 750, done);
+                    transform: 'scale(1, 1)'
+                }, 600, done);
             }else {
                 jQuery(element).css({
                     opacity: 0,
-                    transform: 'scale3d(2, 2, 1)'
+                    transform: 'scale(2, 2)'
                 });
 
                 jQuery(element).animate({
                     opacity: 1,
-                    transform: 'scale3d(1, 1, 1)'
-                }, 750, done);
+                    transform: 'scale(1, 1)'
+                }, 600, done);
             }
         }
     };
